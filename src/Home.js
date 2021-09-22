d262426135ca0a754b5161a8cdb09dcc15c7aff9
kyletower <kyle.tower@gmail.com>
2021-09-22 07:16:04 -0400

    adding blogs as a reactive variable

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,16 @@ const Home = () => {
   const [name, setName] = useState('mario');
   const [age, setAge] = useState(25);
 
+  const [blogs, setBlogs] = useState([
+    { body: 'Come and welcome to Jesus Christ.', author: 'Doug Wilson', id: 1 },
+    { body: 'God is most glorified in you...', author: 'John Piper', id: 2 },
+    {
+      body: 'You are far worse than you realize and yet more loved than you would dare hop',
+      author: 'Tim Keller',
+      id: 3,
+    },
+  ]);
+
   // first param is automatically receiving event
   const handleClick = (e) => {
     console.log('hello, handleClick');
