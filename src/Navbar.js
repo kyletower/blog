a0ced305910ef12026850aba6e4fa4ce123e39c5
kyletower <kyle.tower@gmail.com>
2021-09-21 21:33:00 -0400

    adjusting styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,18 @@
 const Navbar = () => {
+  const style = {
+    color: 'white',
+    backgroundColor: '#f1356d',
+    borderRadius: '8px',
+  };
+
   return (
     <nav className='navbar'>
       <h1>The Metanoia Blog</h1>
       <div className='links'>
         <a href='/'>Home</a>
-        <a href='/create'>New Blog</a>
+        <a href='/create' style={style}>
+          New Blog
+        </a>
       </div>
     </nav>
   );
