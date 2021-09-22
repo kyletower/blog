cc8c7a805faa74873abfd9c3d5dac93f8a120427
kyletower <kyle.tower@gmail.com>
2021-09-22 06:58:16 -0400

    reacting to click events in a component

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,22 @@
 const Home = () => {
-  const handleClick = () => {
+  // first param is automatically receiving event
+  const handleClick = (e) => {
     console.log('hello, ninjas');
+    console.log(e);
   };
 
-  const handleClickAgain = (name) => {
+  const handleClickAgain = (e, name) => {
     console.log(`hello ${name}`);
+    console.log(e);
   };
 
   return (
     <div className='home'>
       <h2>Home Page</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain('mario')}>Click me too!</button>
+      <button onClick={(e) => handleClickAgain(e, 'mario')}>
+        Click me too!
+      </button>
     </div>
   );
 };
