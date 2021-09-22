88ea12d7e5b1a13665bf169aedccd5b70d03ef5c
kyletower <kyle.tower@gmail.com>
2021-09-21 21:14:10 -0400

    adding link to DuckDuckGo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to my blog!';
+  const likes = 37;
+  const link = 'https://duckduckgo.com';
+
   return (
     <div className='App'>
-      <header className='App-header'>header</header>
+      <div className='content'>
+        <h1>{title}</h1>
+        <p>Liked {likes} times.</p>
+        <p>Viewed {Math.random() * 10} times</p>
+        <a href={link}>Duck Duck Go!</a>
+      </div>
     </div>
   );
 }
