a17935a91f23d21b08b8e802e0757cc6a53be208
kyletower <kyle.tower@gmail.com>
2021-09-23 21:43:48 -0400

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -30,10 +31,21 @@ const Home = () => {
     // don't change the state inside useEffect, lest you cause an inf loop
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource.');
+        }
+
         return res.json();
       })
       .then((data) => {
         setBlogs(data); // this won't cause an inf loop because we have an empty dependecy array
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // console.error(err.message);
+        setIsLoading(false);
+        setError(err.message);
       });
   }, []);
 
@@ -65,6 +77,7 @@ const Home = () => {
       </button>
       */
       }
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       <p>{name}</p>
       {blogs && (
