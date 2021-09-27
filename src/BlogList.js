b078ec0d3c331897f2b8b968495e6fa5b8acc86f
kyletower <kyle.tower@gmail.com>
2021-09-27 07:32:26 -0400

    adding BlogDetails and useParams to get the id of a blog. Updating BlogList to use Link to a specific blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const BlogList = ({ blogs, category, handleDelete }) => {
   return (
     <div className='blog-list'>
       <h3>{category}</h3>
       {blogs.map((blog) => (
         <div className='blog-preview' key={blog.id}>
-          <h2>{blog.author}</h2>
-          <p>{blog.body}</p>
-          <p>— {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.author}</h2>
+            <p>{blog.body}</p>
+            <p>— {blog.author}</p>
+          </Link>
+
           <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
