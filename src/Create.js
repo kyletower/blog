847ee044dafba0fa2f87e923dd70d04b92d03b85
kyletower <kyle.tower@gmail.com>
2021-09-27 07:51:48 -0400

    updating Create to include a form with an input, textarea, select options, and button

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,20 @@ const Create = () => {
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type='text' requried />
+
+        <label>Blog Body:</label>
+        <textarea required></textarea>
+
+        <label>Blog Author:</label>
+        <select>
+          <option value='otto'>otto</option>
+          <option value='kyle'>kyle</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
