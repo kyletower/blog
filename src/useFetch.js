4ada98e68b234f5f3309a503911a898fbde4073a
kyletower <kyle.tower@gmail.com>
2021-09-27 06:56:24 -0400

    resolving the JSON parse error caused by shadow copy of the url variable.

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,36 +5,36 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  console.log('url inside useFetch: ', url);
 
-  useEffect(
-    (url) => {
-      // this functions every time there's a render or rerender,
-      // when data loads and when a reactive var changes state
-      // this is the ideal time to fetch so that the back end
-      // matches what the user sees on the front end
-      // fetch data, or communicate with authentication service
-      // don't change the state inside useEffect, lest you cause an inf loop
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('Could not fetch the data for that resource.');
-          }
+  useEffect(() => {
+    // this functions every time there's a render or rerender,
+    // when data loads and when a reactive var changes state
+    // this is the ideal time to fetch so that the back end
+    // matches what the user sees on the front end
+    // fetch data, or communicate with authentication service
+    // don't change the state inside useEffect, lest you cause an inf loop
+    console.log('url inside useEffect: ', url);
 
-          return res.json();
-        })
-        .then((data) => {
-          setData(data); // this won't cause an inf loop because we have an empty dependecy array
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          // console.error(err.message);
-          setIsLoading(false);
-          setError(err.message);
-        });
-    },
-    [url]
-  );
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource.');
+        }
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        setData(data); // this won't cause an inf loop because we have an empty dependecy array
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        console.error(err.message);
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
 
   return { data, isLoading, error };
 };
