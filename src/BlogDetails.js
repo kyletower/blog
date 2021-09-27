478e96c2938512c626ad9d5422765aef16476446
kyletower <kyle.tower@gmail.com>
2021-09-27 07:43:07 -0400

    updating BlogDetails to fetch dynamic url with blog id and display its content

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,28 @@
 import { useParams } from 'react-router';
+import useFetch from './useFetch';
 // import the above from react-router-dom?
 
 const BlogDetails = () => {
   const { id } = useParams();
+  // 'http://localhost:8000/blogs' + id
+  const {
+    data: blog,
+    error,
+    isLoading,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
 
   return (
     <div className='blog-details'>
-      <h2>Blog Details - {id}</h2>
+      {isLoading && <div>Loading...</div>}
+      {error && <div> {error} </div>}
+      {blog && (
+        <article>
+          <h2>{blog.author}</h2>
+          <div>
+            {blog.body} — {blog.author}
+          </div>
+        </article>
+      )}
     </div>
   );
 };
