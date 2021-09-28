07ded00b680d7d4b641db44fe3d4d838f1e46c16
kyletower <kyle.tower@gmail.com>
2021-09-27 21:20:57 -0400

    adding isLoading and disabling the button when isLoading

--- src/Create.js
+++ src/Create.js
@@ -4,13 +4,25 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('otto');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
     console.log('inside handle submit');
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsLoading(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsLoading(false);
+    });
   };
+
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
@@ -22,20 +34,22 @@ const Create = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-
         <label>Blog Body:</label>
         <textarea
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
-
         <label>Blog Author:</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value='otto'>otto</option>
           <option value='kyle'>kyle</option>
         </select>
-        <button>Add Blog</button>
+        {!isLoading ? (
+          <button>Add Blog</button>
+        ) : (
+          <button disabled>Adding Blog...</button>
+        )}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
